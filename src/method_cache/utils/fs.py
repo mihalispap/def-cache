cee5ffe9a9f4c5bc9469640bbe22dbaa17886ecb
Mihalis <mihalispapak@gmail.com>
2025-03-22 18:12:19 +0200

    add fs helper methods

--- src/method_cache/utils/fs.py
+++ src/method_cache/utils/fs.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+from typing import Optional
+
+
+def file_exists(filepath: str) -> bool:
+    return Path(filepath).is_file()
 
 
 def directory_exists(dirpath: str) -> bool:
@@ -7,3 +12,11 @@ def directory_exists(dirpath: str) -> bool:
 
 def create_directory(dirpath: str) -> None:
     Path(dirpath).mkdir(parents=True, exist_ok=True)
+
+
+def get_file_timestamp(filepath: str) -> Optional[float]:
+    return Path(filepath).stat().st_mtime
+
+
+def safe_remove_file(filepath: str):
+    Path(filepath).unlink(missing_ok=True)
